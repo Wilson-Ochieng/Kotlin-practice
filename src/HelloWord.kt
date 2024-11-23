dc84ca36e566bbd4e6d6703e428b773673c92d15
Wilson-Ochieng <wilsonochieng718@gmail.co m>
2024-11-23 11:36:15 +0300

    Inserted changes

---
+++ src/HelloWord.kt
@@ -0,0 +1,8 @@
+fun main ( ){
+    val x=20;
+    val y=30;
+    val sum =x+y;
+    println(sum)
+
+
+}
---
+++ src/Variables.kt
@@ -0,0 +1,7 @@
+fun main (){
+    val name = "My name is Wilson"
+//     name = "My name is Jane";
+//     name ="Joseph"
+
+    println(name)
+}
