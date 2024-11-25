2bc08414ed1f80c439fae14400acc1d7a8de2b40
Wilson-Ochieng <wilsonochieng718@gmail.co m>
2024-11-25 15:30:39 +0300

    using named arguments in called birthdayGreeting

--- src/FunctionsPractice.kt
+++ src/FunctionsPractice.kt
@@ -1,13 +1,12 @@
 fun main(){
-    birthdayGreeting("Roy",9)
-    println(birthdayGreeting("Roy",9))
+    //Using Named arguments
+    val greeting =birthdayGreeting(name="Angel",age=89,isKenyan = true)
+    println(greeting)
 
 }
-
-
 //Passed multiple  parameters
-fun birthdayGreeting(name:String,age:Int): String {
+fun birthdayGreeting(name:String,age:Int,isKenyan:Boolean): String {
     val nameGreeting="Happy Birthday, $name!"
-    val ageGreeting ="You are now $age years old!"
+    val ageGreeting ="You are now $age years old and you are $isKenyan!"
     return "$nameGreeting\n ,$ageGreeting"
 }
